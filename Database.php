08a865e714f220b8751b92d87bab1b257c0fcca2
Haziel Sanchez <hazieldev12@gmail.com>
2026-07-13 12:14:23 -0500

    Se agregaron comentarios

--- Database.php
+++ Database.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 
 function getConnection(): PDO{
-    $dsn = "mysql:host=localhost;dbname=nexus_gaming_php;charset=utf8mb4;port=3306";
+    $dsn = "mysql:host=localhost;dbname=nexus_gaming_php;charset=utf8mb4";
     $username = "root";
     $password = "";
     return new PDO($dsn, $username, $password,
     [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false //Protección para sql inyection
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //si ocurre un error, envía una Exception en lugar de solo emitir un warning silencioso
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //Define que por defecto, cuando se hace un fetch() o fetchAll(), los resultados se devuelvan como arreglos asociativos
+        PDO::ATTR_EMULATE_PREPARES => false //Desactiva la emulación de consultas preparadas y obliga a PDO a usar prepared statements reales del motor MySQL.
     ]);
 }
