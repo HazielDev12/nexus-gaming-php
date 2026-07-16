f1eeaaf743d3057befe9c0f4bccd108a629fabec
Haziel Sanchez <hazieldev12@gmail.com>
2026-07-16 12:28:54 -0500

    Nuevos archivos creados

--- src/Http/Helpers.php
+++ src/Http/Helpers.php
@@ -1,33 +1,11 @@
 <?php
-function resolveRoute(array $segments): array{
-    //Buscar la palabra "games" dentro del arreglo ($segments)
-    $pos = array_search("games", $segments, true);
-//    0 => api-games
-//    1 => games
-//    2 => 5
 
-    if($pos === false){
-        return [null, null];
-    }
 
-    $resource = "games";
-    $id = $segments[$pos+1] ?? null;
-    //Verifica si vino un id
-    if($id !== null){
-        //ctype_digit = ¿Todos los caracteres son números?
-        if(!ctype_digit($id)){
-            respondError(400, "El id debe ser numérico");
-        }
+namespace App\Helpers;
 
-        return [$resource, (int)$id]; // Convierte "5" en 5 ((int)$id).
-        //Devuelve
-//        [
-//            "games",
-//            5
-//        ]
-    }
-    return [$resource, null];
-}
+
+class Response{
+
 //==================================================
 //Devolver errores de forma mas "elegante"
 #[NoReturn]
@@ -82,6 +60,7 @@ function readJsonBody(): array
     //Devolver el resultado
     return $data;
 }
+
 
 function validateProductPayload(array $data, bool $isCreate, bool $requireAllFields = false): array
 {
@@ -166,4 +145,5 @@ function mergedGameData(array $existing, array $payload): array{
     }
 
     return $merged;
+}
 }
