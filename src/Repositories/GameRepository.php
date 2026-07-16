8d4be4585d1f20b7a3a41b778a856d45d205002b
Haziel Sanchez <hazieldev12@gmail.com>
2026-07-16 11:47:39 -0500

    Se volvió clase

--- src/Repositories/GameRepository.php
+++ src/Repositories/GameRepository.php
@@ -1,51 +1,60 @@
 <?php
 
+namespace App\Repositories;
 
+use PDO;
 
-function getAllGames(PDO $pdo): array{
-    $sql = "SELECT id,name,price,status,created_at FROM games ORDER BY id DESC";
-    $stmt = $pdo->query($sql);
-    return $stmt->fetchAll();
-}
+class GameRepository{
 
-function getGameById(PDO $pdo, int $id): ?array{
-    $sql = "SELECT id,name,price,status,created_at FROM games WHERE id= :id";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(["id"=>$id]);
-    $game = $stmt->fetch();
-    return $game !== false ? $game : null;
-}
-function createGame(PDO $pdo, array $data): int{
-    $sql = "INSERT INTO games (name,price,status) VALUES (:name,:price,:status)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(
-        [
-            "name" => trim((string)$data["name"]),
-            "price" => (float)$data["price"],
-            "status" => trim((string)$data["status"] ?? "available")
-        ]);
-    return (int)$pdo->lastInsertId();
-}
+    public function __construct(
+        private PDO $pdo
+    ){}
 
-function updateGame(PDO $pdo, int $id, array $data): bool{
-    $sql = "UPDATE games SET name= :name,price= :price,status= :status WHERE id= :id";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(
-        [
-            "id"=>$id,
-            "name" => trim((string)$data["name"]),
-            "price" => (float)$data["price"],
-            "status" => trim((string)$data["status"])
-        ]);
-    return $stmt->rowCount() > 0;
-}
+    public function getAllGames(): array{
+        $sql = "SELECT id,name,price,status,created_at FROM games ORDER BY id DESC";
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll();
+    }
 
-function deleteGame(PDO $pdo, int $id): bool{
-    $sql = "DELETE FROM games WHERE id= :id";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(
-        [
-            "id"=>$id,
-        ]);
-    return $stmt->rowCount() > 0;
+    public function getGameById(int $id): ?array{
+        $sql = "SELECT id,name,price,status,created_at FROM games WHERE id= :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(["id"=>$id]);
+        $game = $stmt->fetch();
+        return $game !== false ? $game : null;
+    }
+    public function createGame(array $data): int{
+        $sql = "INSERT INTO games (name,price,status) VALUES (:name,:price,:status)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(
+            [
+                "name" => trim((string)$data["name"]),
+                "price" => (float)$data["price"],
+                "status" => trim((string)$data["status"] ?? "available")
+            ]);
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    public function updateGame(int $id, array $data): bool{
+        $sql = "UPDATE games SET name= :name,price= :price,status= :status WHERE id= :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(
+            [
+                "id"=>$id,
+                "name" => trim((string)$data["name"]),
+                "price" => (float)$data["price"],
+                "status" => trim((string)$data["status"])
+            ]);
+        return $stmt->rowCount() > 0;
+    }
+
+    public function deleteGame(int $id): bool{
+        $sql = "DELETE FROM games WHERE id= :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(
+            [
+                "id"=>$id,
+            ]);
+        return $stmt->rowCount() > 0;
+    }
 }
