c8ab2a6c2f294a647bf73742cc41c60faf2d4a63
Haziel Sanchez <hazieldev12@gmail.com>
2026-07-16 12:36:27 -0500

    Se movio readJsonBody a Request.php

---
+++ src/Http/Request.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\src\Http;
+
+class Request{
+
+    function readJsonBody(): array
+    {
+        //Leer el cuerpo crudo de la petición
+        $raw = file_get_contents("php://input"); // "php://input": Stream especial que PHP proporciona para acceder al cuerpo de la petición HTTP.
+//    $raw = '{
+//    "name":"Minecraft",
+//    "price":500
+//    }';
+
+        if ($raw === false || trim($raw) === '') {
+            respondError(400, "El cuerpo de la petición está vacío");
+        }
+
+        //Convertir JSON a PHP
+        $data = json_decode($raw, true); //Sin el true, obtendría un objeto en vez de un arreglo.
+        //Convierte:
+//    [
+//       "name"=>"Mouse",
+//       "price"=>25
+//    ]
+//    a esto:
+//    [
+//      "name"=>"Mouse",
+//      "price"=>25
+//    ]
+//    Ya no es texto, hora es un arreglo de PHP.
+
+        //Verificar si hubo algún error en la decodificación.
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            respondError(400, "JSON inválido " . json_last_error_msg());
+        }
+        //Verificar el tipo de dato, es decir, que el JSON represente un objeto.
+        if (!is_array($data)) {
+            respondError(400, "El JSON debe representar un objeto");
+        }
+
+        //Devolver el resultado
+        return $data;
+    }
+
+
+
+}
--- src/Http/Response.php
+++ src/Http/Response.php
@@ -1,10 +1,8 @@
 <?php
 
-namespace App\src\Http\Response;
+namespace App\src\Http;
 
 class Response{
-
-//==================================================
 //Devolver errores de forma mas "elegante"
 #[NoReturn]
 function respondJson(int $statusCode, $payload): void{
@@ -18,47 +16,6 @@ function respondJson(int $statusCode, $payload): void{
 function respondError(int $statusCode, string $message): void{
     respondJson($statusCode, ["error"=>$message]);
 }
-
-function readJsonBody(): array
-{
-    //Leer el cuerpo crudo de la petición
-    $raw = file_get_contents("php://input"); // "php://input": Stream especial que PHP proporciona para acceder al cuerpo de la petición HTTP.
-//    $raw = '{
-//    "name":"Minecraft",
-//    "price":500
-//    }';
-
-    if ($raw === false || trim($raw) === '') {
-        respondError(400, "El cuerpo de la petición está vacío");
-    }
-
-    //Convertir JSON a PHP
-    $data = json_decode($raw, true); //Sin el true, obtendría un objeto en vez de un arreglo.
-    //Convierte:
-//    [
-//       "name"=>"Mouse",
-//       "price"=>25
-//    ]
-//    a esto:
-//    [
-//      "name"=>"Mouse",
-//      "price"=>25
-//    ]
-//    Ya no es texto, hora es un arreglo de PHP.
-
-    //Verificar si hubo algún error en la decodificación.
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        respondError(400, "JSON inválido " . json_last_error_msg());
-    }
-    //Verificar el tipo de dato, es decir, que el JSON represente un objeto.
-    if (!is_array($data)) {
-        respondError(400, "El JSON debe representar un objeto");
-    }
-
-    //Devolver el resultado
-    return $data;
-}
-
 
 function validateProductPayload(array $data, bool $isCreate, bool $requireAllFields = false): array
 {
